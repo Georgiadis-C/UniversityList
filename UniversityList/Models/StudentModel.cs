04bfa3ffebc1f19d127241b89470e3f8029c3316
Christos Georgiadis <chrisg.e.o.rgiadis420@gmail.com>
2026-05-14 14:05:41 +0300

    Add validation and error display to student form
    Added data annotation validation to StudentModel for all fields. Updated AddUpdateStudentDetailViewModel to perform validation and expose error messages. Modified AddUpdateStudentDetail.xaml to show validation errors under each input and set Entry text color to white.

--- UniversityList/Models/StudentModel.cs
+++ UniversityList/Models/StudentModel.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
-using SQLite;
 
 namespace UniversityList.Models
 {
@@ -9,11 +10,33 @@ namespace UniversityList.Models
     {
         [PrimaryKey, AutoIncrement]
         public int StudentId { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "First name can only contain letters")]
+        [StringLength(20, MinimumLength = 2)]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Last name can only contain letters")]
+        [StringLength(20, MinimumLength = 2)]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        [RegularExpression(@"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*@(gmail\.com|outlook\.com|yahoo\.com)$",
+        ErrorMessage = "Only Gmail, Outlook or Yahoo accounts are allowed")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Department is required")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Department can only contain letters")]
         public string Department { get; set; }
-        public int? YearAdded { get; set; }   
+
+        [Required(ErrorMessage = "Year added is required")]
+        [Range(1975, 2026, ErrorMessage = "Year added must be between 1975 and 2026")]
+        public int? YearAdded { get; set; }
+
+        [Required(ErrorMessage = "GPA is required")]
+        [Range(5.0, 10.0, ErrorMessage = "GPA must be between 5.0 and 10.0")]
         public double? GPA { get; set; }
 
     }
--- UniversityList/ViewModels/AddUpdateStudentDetailViewModel.cs
+++ UniversityList/ViewModels/AddUpdateStudentDetailViewModel.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using CommunityToolkit.Mvvm.Messaging;
+using System.ComponentModel.DataAnnotations;
 using UniversityList.Interfaces;
 using UniversityList.Models;
 
@@ -14,17 +12,70 @@ namespace UniversityList.ViewModels
     {
         [ObservableProperty]
         StudentModel _studentDetail = new();
+        
+        [ObservableProperty]
+        string firstNameError; 
+        [ObservableProperty]
+        string lastNameError;
+        [ObservableProperty]
+        string emailError;
+        [ObservableProperty]
+        string departmentError;
+        [ObservableProperty]
+        string yearAddedError;
+        [ObservableProperty]
+        string gpaError;
 
         [RelayCommand]
         public async Task AddUpdateStudent()
         {
+            FirstNameError = LastNameError = EmailError = DepartmentError = YearAddedError = GpaError = string.Empty;
+
+            var context = new ValidationContext(StudentDetail);
+            var results = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(StudentDetail, context, results, true);
+
+            if (!isValid)
+            {
+                foreach (var error in results)
+                {
+                    var propertyName = error.MemberNames.FirstOrDefault();
+
+                    switch (propertyName)
+                    {
+                        case nameof(StudentDetail.FirstName):
+                            FirstNameError = error.ErrorMessage;
+                            break;
+                        case nameof(StudentDetail.LastName):
+                            LastNameError = error.ErrorMessage;
+                            break;
+                        case nameof(StudentDetail.Email):
+                            EmailError = error.ErrorMessage;
+                            break;
+                        case nameof(StudentDetail.Department):
+                            DepartmentError = error.ErrorMessage;
+                            break;
+                        case nameof(StudentDetail.YearAdded):
+                            YearAddedError = error.ErrorMessage;
+                            break;
+                        case nameof(StudentDetail.GPA):
+                            GpaError = error.ErrorMessage;
+                            break;
+                    }
+                }
+                return; 
+            }
+
             await studentService.SaveStudent(StudentDetail);
 
             if (Shell.Current != null)
             {
                 await Shell.Current.DisplayAlertAsync("Student Info Saved", "Record Saved", "OK");
 
+                await Shell.Current.GoToAsync("..");
             }
+
             StudentDetail = new();
         }
     }
